dd1db5f9e994de1b6471f388227e5e86f153a073
putuwidyaastuti88 <putuwidyaastuti88@gmail.com>
2024-11-19 00:31:47 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Menambahkan efek scroll ke bagian tertentu
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
